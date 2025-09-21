44e383c7a8e9432551def1027e7c4f97b1d2c81f
mateuskolap <mateuskolap@gmail.com>
2025-09-21 00:24:24 -0300

    feat: lógica de criação de usuários com diferentes níveis de acesso

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -12,21 +12,21 @@ class CourseController extends Controller
 {
     public function index(Request $request)
     {
-        $validated = $request->validate([
+        $request->validate([
             'organization_id' => ['nullable', 'exists:organizations,id'],
             'name' => ['nullable', 'string'],
             'coordinator_id' => ['nullable', 'exists:coordinators,id'],
         ]);
 
         $courses_list = Course::query()
-            ->when(!empty($validated['organization_id']), function ($query) use ($validated) {
-                $query->where('organization_id', $validated['organization_id']);
+            ->when(!empty($request['organization_id']), function ($query) use ($request) {
+                $query->where('organization_id', $request['organization_id']);
             })
-            ->when(!empty($validated['name']), function ($query) use ($validated) {
-                $query->where('name', 'like', '%' . $validated['name'] . '%');
+            ->when(!empty($request['name']), function ($query) use ($request) {
+                $query->where('name', 'like', '%' . $request['name'] . '%');
             })
-            ->when(!empty($validated['coordinator_id']), function ($query) use ($validated) {
-                $query->where('coordinator_id', $validated['coordinator_id']);
+            ->when(!empty($request['coordinator_id']), function ($query) use ($request) {
+                $query->where('coordinator_id', $request['coordinator_id']);
             })
             ->select(['id', 'name', 'organization_id', 'coordinator_id'])
             ->with(['coordinator.user:id,name', 'organization:id,name'])
@@ -46,26 +46,26 @@ class CourseController extends Controller
 
     public function store(Request $request)
     {
-        $validated = $request->validate([
+        $request->validate([
             'name' => ['required', 'string'],
             'organization_id' => ['required', 'exists:organizations,id'],
             'coordinator_id' => ['required', 'exists:coordinators,id']
         ]);
 
-        Course::create($validated);
+        Course::create($request);
 
         return redirect()->back()->with('success', 'Curso criado com sucesso!');
     }
 
     public function update(Request $request, Course $course)
     {
-        $validated = $request->validate([
+        $request->validate([
             'name' => ['required', 'string'],
             'organization_id' => ['required', 'exists:organizations,id'],
             'coordinator_id' => ['required', 'exists:coordinators,id']
         ]);
 
-        $course->update($validated);
+        $course->update($request);
 
         return redirect()->back()->with('success', 'Curso atualizado com sucesso!');
     }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Coordinator;
+use App\Models\Professor;
+use App\Models\Student;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rule;
 use Illuminate\Validation\Rules;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -18,18 +19,18 @@ class UserController extends Controller
 {
     public function index(Request $request): Response
     {
-        $validated = $request->validate([
+        $request->validate([
             'name' => ['nullable', 'string'],
             'role_id' => ['nullable', 'exists:roles,id'],
         ]);
 
         $users_list = User::query()
-            ->when(!empty($validated['name']), function ($query) use ($validated) {
-                $query->where('users.name', 'like', '%' . $validated['name'] . '%');
+            ->when(!empty($request['name']), function ($query) use ($request) {
+                $query->where('users.name', 'like', '%' . $request['name'] . '%');
             })
-            ->when(!empty($validated['role_id']), function ($query) use ($validated) {
-                $query->whereHas('roles', function ($query) use ($validated) {
-                    $query->where('roles.id', $validated['role_id']);
+            ->when(!empty($request['role_id']), function ($query) use ($request) {
+                $query->whereHas('roles', function ($query) use ($request) {
+                    $query->where('roles.id', $request['role_id']);
                 });
             })
             ->with('roles:id,name')
@@ -44,17 +45,13 @@ class UserController extends Controller
         ]);
     }
 
-    /**
-     * Handle an incoming registration request.
-     *
-     * @throws \Illuminate\Validation\ValidationException
-     */
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|lowercase|email|max:255|unique:' . User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'role_id' => ['required', 'exists:roles,id'],
         ]);
 
         $user = User::create([
@@ -63,11 +60,26 @@ class UserController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
+        $role = Role::findById($request['role_id']);
+        $user->assignRole($role->name);
+
+        switch ($role->name) {
+            case 'student':
+                Student::create(['user_id' => $user->id]);
+                break;
+
+            case 'coordinator':
+                Coordinator::create(['user_id' => $user->id,]);
+                break;
+
+            case 'professor':
+                Professor::create(['user_id' => $user->id,]);
+                break;
+        }
+
         event(new Registered($user));
 
-        Auth::login($user);
-
-        return to_route('dashboard');
+        return redirect()->back()->with('success', 'Usuário cadastrado com sucesso');
     }
 
     public function destroy(User $user)
