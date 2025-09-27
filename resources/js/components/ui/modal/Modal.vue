fbd45db0cccb3d99ddabdbcdb0a5c442732b9324
mateuskolap <mateuskolap@gmail.com>
2025-09-27 00:36:00 -0300

    fix: adiciona cursor pointer para fechar modal

--- resources/js/components/ui/modal/Modal.vue
+++ resources/js/components/ui/modal/Modal.vue
@@ -70,7 +70,7 @@ onMounted(() => {
                         </h3>
                         <button
                             type="button"
-                            class="text-gray-300 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                            class="cursor-pointer text-gray-300 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                             @click="closeModal"
                         >
                             <svg aria-hidden="true"
